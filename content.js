884093f904b771ed8033751ad19814ca6dcc8ba0
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-03-08 05:38:51 +0100

    liking to gitbook

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'TS-EVT',
   subtitle: "Type safe replacement for node's EventEmitter that challenge RxJS",
   description: "TS-EVT is a type safe replacement for EventEmitter that challenge RxJS and run everywhere, Node.JS, Deno, the browser of your grandma and ReactNative",
-  docUrl: "https://github.com/garronej/ts-evt",
+  docUrl: "https://docs.ts-evt.dev",
   githubUrl: "https://github.com/garronej/ts-evt",
   runItUrl: "https://stackblitz.com/edit/ts-evt-demo-hello-world?embed=1&file=index.ts"
 }
