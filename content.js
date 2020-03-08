e79afa80da0424cf50e051b56527f79c1b3e4c4c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-03-08 05:19:29 +0100

    Good enough

--- content.js
+++ content.js
@@ -1,37 +1,8 @@
 module.exports = {
   title: 'TS-EVT',
-  subtitle: "Type safe replacement for node's EventEmitter that deprecate RxJS",
-  description: 'FoalTS is a complete Node.js and TypeScript framework with everything you need to create server-side applications. Its architecture keeps things simple and intuitive.',
-  features: [
-    {
-      name: 'Production-ready',
-      icon: 'ship',
-      description: 'FoalTS provides an advanced configuration system to smoothly switch between your environments. How you specify the configuration may change between deploys (env variables, json files, etc). But your codebase stays the same.'
-    },
-    {
-      name: 'TypeScript',
-      icon: 'code',
-      description: 'TypeScript brings you optional static type-checking along with the latest ECMAScript features. Writing FoalTS with TypeScript has been and will always be fundamental. Code is more elegant and concise. Most of the silly mistakes are caught at compilation. And autocompletion is well-handled and the API is better documented.'
-    },
-    {
-      name: 'Dev tools',
-      icon: 'wrench',
-      description: 'Every project needs dev tools. But setting up and maintaining such an environment in Node.Js is often hard or time consuming. FoalTS focuses on helping the developer to code. Generators, watchers, the compiler, linter, bundler and test framework are all provided in FoalTS. No more set up, get started right away.',
-    },
-    {
-      name: 'Basic components',
-      icon: 'cubes',
-      description: 'Some patterns always come back while developing a web app. Set up a REST API, talk to the database or manage authentication shouldn\'t be a hard task. FoalTS lets you focus on the business logic, not on re-inventing the wheel.'
-    },
-    {
-      name: 'Architecture',
-      icon: 'home',
-      description: 'FoalTS provides a robust architecture for creating large web apps. Divided into controllers, services and hooks, your code is well organized. FoalTS enforces the separation of concerns, improves testability and makes your code more readable.',
-    },
-    {
-      name: 'Security',
-      icon: 'shield',
-      description: 'Security is everyone\'s business. To help you ship secure apps FoalTS provides some common tools (csrf, xss protections) to take care of it. Moreover all default configurations are set to prevent security failures (cookies, headers, etc).',
-    },
-  ]
+  subtitle: "Type safe replacement for node's EventEmitter that challenge RxJS",
+  description: "TS-EVT is a type safe replacement for EventEmitter that challenge RxJS and run everywhere, Node.JS, Deno, the browser of your grandma and ReactNative",
+  docUrl: "https://docs.ts-evt.dev",
+  githubUrl: "https://github.com/garronej/ts-evt",
+  runItUrl: "https://stackblitz.com/edit/ts-evt-demo-hello-world?embed=1&file=index.ts"
 }
