a9d47faeb4de15b7f540542c9ad368b240c512b7
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-03-13 20:18:39 +0100

    migrate to evt.land

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
 
 module.exports = {
-  title: 'TS-EVT',
+  title: 'EVT',
   subtitle: "A type safe replacement for node's EventEmitter that challenge RxJS",
-  description: "TS-EVT is a type safe replacement for EventEmitter that challenge RxJS and run everywhere, Node.JS, Deno, the browser of your grandma and ReactNative",
-  docUrl: "https://docs.ts-evt.dev",
-  githubUrl: "https://github.com/garronej/ts-evt",
-  runItUrl: "https://stackblitz.com/edit/ts-evt-demo-hello-world?embed=1&file=index.ts"
+  description: "EVT is a type safe replacement for EventEmitter that challenge RxJS and run everywhere, Node.JS, Deno, the browser of your grandma and ReactNative",
+  apiUrl: "https://docs.evt.land/api",
+  learnMoreUrl: "https://docs.evt.land",
+  githubUrl: "https://github.com/garronej/evt",
+  stackblitzUrl: "https://stackblitz.com/edit/ts-evt-demo-hello-world?embed=1&file=index.ts"
 }
