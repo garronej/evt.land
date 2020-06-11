828f40b4bc77e16c61d68d991b5fce4c558ab06e
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-06-11 17:14:46 +0200

    use templating engine for quotes

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   title: 'EVT',
   subtitle: "EventEmitter's typesafe replacement",
@@ -8,5 +7,25 @@ module.exports = {
   apiUrl: "https://docs.evt.land/api",
   learnMoreUrl: "https://docs.evt.land",
   githubUrl: "https://github.com/garronej/evt",
-  stackblitzUrl: "https://stackblitz.com/edit/evt-playground?embed=1&file=index.ts&hideExplorer=1"
+  stackblitzUrl: "https://stackblitz.com/edit/evt-playground?embed=1&file=index.ts&hideExplorer=1",
+  "quotes": [
+    {
+      "logo": "assets/img/logo_connext.png",
+      "url": "https://connext.network",
+      "quote": `At <a target="_blank" rel="noopener noreferrer" href="https://connext.network">Connext</a>, we are building complex, distributed systems that rely heavily on event-driven processes. 
+We eventually found that the capabilities of the Node.js EventEmitter could not handle some of our more complex event-based needs 
+such as attaching an event based on a filter. 
+We are grateful to have found EVT as a suitable replacement which gives us strong typing and a powerful API for all of our eventing needs. 
+We were able to eliminate a lot of boilerplate and complicated code around resolving promises and detaching listeners by using waitFor. 
+We have also exposed the EVT interface through our API so our users can make use of these powerful features.`,
+      "author": `Rahul Sethuram, CTO at <a target="_blank" rel="noopener noreferrer" href="https://connext.network">Connext</a>`
+    },
+    {
+      "logo": "assets/img/logo_semasim.png",
+      "url": "https://www.semasim.com",
+      "quote": `EVT catches at compile time countless errors that other event management libraries would have let slip through. 
+If forces you to write code in ways TypeScript can understand so you always know what is what.`,
+      "author": `William Thiery, Frontend Dev at <a target="_blank" rel="noopener noreferrer" href="https://semasim.com">Semasim</a>`
+    }
+  ]
 }
